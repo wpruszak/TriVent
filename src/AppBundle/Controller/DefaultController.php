9e470c39f033f994a0662296aa81d81a19fd226b
Wojciech Pruszak <info@wpruszak.com>
2017-05-28 01:44:13 +0200

    wip

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Activity;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,29 +15,50 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-//        $entityManager = $this->getDoctrine()->getManager();
-//        $activity = new Activity();
-//        $activity->setDateFrom(new \DateTime());
-//        $activity->setDateTo(new \DateTime());
-//        $activity->setDescription('Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed, vestibulum id, eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non, consectetuer lobortis quis, varius in, purus. Integer ultrices posuere cubilia Curae, Nulla ipsum dolor lacus, suscipit adipiscing. Cum sociis natoque penatibus et ultrices volutpat. Nullam wisi ultricies a, gravida vitae, dapibus risus ante sodales lectus blandit eu, tempor diam pede cursus vitae, ultricies eu, faucibus quis, porttitor eros cursus lectus, pellentesque eget, bibendum a, gravida ullamcorper quam. Nullam viverra consectetuer. Quisque cursus et, porttitor risus. Aliquam sem. In hendrerit nulla quam nunc, accumsan congue. Lorem ipsum primis in nibh vel risus. Sed vel lectus. Ut sagittis, ipsum dolor quam.');
-//        $activity->setLink('#');
-//        $activity->setTitle('Lorem ipsum dolor tit emet.');
-//        $activity->setPubDate(new \DateTime());
-//        $activity->setPlace('84-240 Reda, Obwodowa 7/4');
-//
-//        $entityManager->persist($activity);
-//        $entityManager->flush();
-
         return $this->render('default/index.html.twig');
     }
+
     /**
      * @Route("/list", name="default_list")
      */
     public function listAction(Request $request)
     {
         $activities = $this->getDoctrine()->getRepository(Activity::class)->findAll();
+
+        $activityQuery = $this->getDoctrine()
+            ->getRepository(Activity::class)
+            ->createQueryBuilder('a')
+            ->getQuery();
+
+        $pagination = $this->get('knp_paginator')->paginate(
+            $activityQuery,
+            $request->get('page') ?? 1,
+            12
+        );
+
         return $this->render('default/list.html.twig', [
-            'activities' => $activities
+            'activities' => $activities,
+            'pagination' => $pagination
         ]);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @Route("/paginate", name="default_paginate")
+     * @Method("POST")
+     */
+    public function paginationAction(Request $request)
+    {
+        $activityQuery = $this->getDoctrine()
+            ->getRepository(Activity::class)
+            ->createQueryBuilder('a')
+            ->getQuery();
+
+        $paginator = $this->get('knp_paginator')->paginate(
+            $activityQuery,
+            $request->get('page') ?? 1,
+            12
+        );
     }
 }
